3854880ff46c64c4967aa789fbf537339282fada
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-25 13:22:27 +0800

    initial commit

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,8 @@
+// PART 1 import libraries we need to create a component
+
+
+// PART 2 create a component - a function the returns some JSX
+
+// PART 3 create a StyleSheet to style our component
+
+// PART 4 export the component so we can use it else where in our project
