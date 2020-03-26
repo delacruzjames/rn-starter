f3ab0c6bc133a51549807ad072bf705f76407c30
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-26 14:31:30 +0800

    Completed section3

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,34 @@
+// PART 1 import libraries we need to create a component
+import React from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+
+const ListScreen = () => {
+  const friends = [
+    {name: "friend #1", age: "30" },
+    {name: "friend #2", age: "23" },
+    {name: "friend #3", age: "23" },
+    {name: "friend #4", age: "28" },
+    {name: "friend #5", age: "24" }
+  ]
+  return (
+    <FlatList
+      keyExtractor={friend => friend.name }
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
+      }}
+    />
+    // <View>
+    //   <Text>ListScreen</Text>
+    // </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 15,
+    marginVertical: 20
+  }
+});
+
+export default ListScreen;
