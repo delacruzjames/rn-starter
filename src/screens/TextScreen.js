ce687c36c2d9a1725666c5645ed2b185543064f1
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-03-29 15:19:27 +0800

    Exercise add password with validations

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
   return (
     <View>
-      <Text>Enter Name: </Text>
+      <Text>Enter Password: </Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        onChangeText={newNameVal => setName(newNameVal)}
+        value={password}
+        onChangeText={newPasswordVal => setPassword(newPasswordVal)}
       />
-      <Text>Your name is {name}</Text>
+      { password.length <= 6 ? <Text>Password must be more than 6 character</Text> : null}
     </View>
   );
 };
